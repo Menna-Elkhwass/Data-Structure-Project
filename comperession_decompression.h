c483b3ae4240548fc89beb1d5b40ea548c6c6ad4
Menna-Elkhwass <105253678+Menna-Elkhwass@users.noreply.github.com>
2023-01-30 01:19:07 +0200

    Create comperession_decompression.h

---
+++ comperession_decompression.h
@@ -0,0 +1,7 @@
+#ifndef COMPERESSION_DECOMPRESSION_H
+#define COMPERESSION_DECOMPRESSION_H
+#include <QMainWindow>
+using namespace std;
+void compress(string input_file,string compressed_file);
+void decode(string compressed_file, string decompressed_file );
+#endif // COMPERESSION_DECOMPRESSION_H
