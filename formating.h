1336aff607af84f39505c49bd1475a0830673336
ShahdAhmed1900719 <105251955+ShahdAhmed1900719@users.noreply.github.com>
2023-02-03 16:47:40 +0200

    Add files via upload

---
+++ formating.h
@@ -0,0 +1,11 @@
+#ifndef FORMATING_H
+#define FORMATING_H
+
+#include <QMainWindow>
+using namespace std;
+string remove_space(string s);
+bool findInOneLine(string s);
+string printsp(int n,string st);
+void formating(string unformated ,string formated);
+
+#endif // FORMATING_H
